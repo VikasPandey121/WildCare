5b3ce0caf85e87d507e511f5ad53e4b2ae956f6b
VikasPandey121 <12.vikaskumar99@gmail.com>
2020-02-01 09:16:09 +0530

    resolved Time finish Bug

---
+++ app/src/main/java/com/vikaspandey121/hackvsit/Time_Up.java
@@ -0,0 +1,14 @@
+package com.vikaspandey121.hackvsit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Time_Up extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_time__up);
+    }
+}
