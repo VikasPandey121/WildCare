b574c3eb3deb52523350b7f77d3473294fe99438
Harsh Tyagi <48311361+HarshTyagi007@users.noreply.github.com>
2020-02-01 06:43:36 +0530

    gamequiz

---
+++ app/src/main/java/com/vikaspandey121/hackvsit/gameQuiz.java
@@ -0,0 +1,14 @@
+package com.vikaspandey121.hackvsit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class gameQuiz extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_quiz);
+    }
+}
